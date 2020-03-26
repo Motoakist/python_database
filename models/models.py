f24889cc842954b4ba6821c416a465b66b618a82
Motoakist <45988963+Motoakist@users.noreply.github.com>
2020-03-26 10:54:44 +0900

    部屋一つ作った

--- models/models.py
+++ models/models.py
@@ -18,5 +18,12 @@ class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     fname = db.Column(db.String(255), nullable=False)
     lname = db.Column(db.String(255), nullable=False)
-    # mail = db.Column(db.String(255), nullable=False)
+    username = db.Column(db.String(255), nullable=False)
     login_id = db.Column(db.Integer, db.ForeignKey('login.id'), nullable=False)
+
+class RoomAl1(db.Model):
+    __tablename__ = 'roomAl1'
+
+    id = db.Column(db.Integer,primary_key=True)
+    sentence = db.Column(db.String(300), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
