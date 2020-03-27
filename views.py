de6193392e17e97d52d3b601c809e306417eb37e
Motoakist <45988963+Motoakist@users.noreply.github.com>
2020-03-27 10:40:59 +0900

    画像表示できるようになったが、コメントだけだと無理

--- views.py
+++ views.py
@@ -3,7 +3,9 @@ from .app import app
 from flask import Flask, render_template, request, redirect, session, url_for
 # url_for generates url (if you want to use css or codes which you made on your own)
 from .models import User, Login, RoomAl1
+import os
 
+app.config["IMAGE_UPLOADS"] = 'static/img'
 @app.route('/', methods=['POST','GET'])
 def index():
     # if request.method == 'POST':
@@ -34,6 +36,8 @@ def index():
         print("Al1usser",Al1user)
     Al1user = list(set(Al1user))
     print(Al1user)
+
+
     return render_template('index.html',user=user,Al1user=Al1user)
 
 @app.route('/signup',methods=['POST', 'GET'])
@@ -88,6 +92,7 @@ def login():
     else:
         return render_template('login.html')
 
+
 @app.route('/roomAlgebra', methods=['POST','GET'])
 def roomAlgebra():
     login_id = session['id']
@@ -95,14 +100,13 @@ def roomAlgebra():
     roomAl1 = RoomAl1.query.all()
     # roomAl1 = Login.query(login).join(roomAl1).filter(login.id==roomAl1.login_id)
     print("ルーム一覧",roomAl1)
-    # images = request.files['image']
-    # pic_name = images.filename
-    # print(pic_name)
     
-
     if request.method == 'POST':
-        roomAl1 = RoomAl1(sentence=request.form['sentence'], uname=request.form['uname'],login_id=request.form['user_id'])
-        print("さとも",roomAl1)
+        images = request.files['image']
+        pic_name = images.filename
+        print(pic_name)
+        images.save(os.path.join(app.config["IMAGE_UPLOADS"], images.filename))
+        roomAl1 = RoomAl1(sentence=request.form['sentence'], uname=request.form['uname'],login_id=request.form['user_id'],pic=pic_name)
         # try:
         db.session.add(roomAl1)
         db.session.commit()
